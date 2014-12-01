a0a68f5e127ee1427a8df6cf2b9db7e6436c7f14
bculkin2442 <bjculkin@mix.wvu.edu>
2014-12-01 18:52:31 -0500

    Very first commit for chorder. Basic command loop FTW.

--- chorder/main.c
+++ chorder/main.c
@@ -2,14 +2,38 @@
 //  main.c
 //  chorder
 //
+//  Produce letters from short sequences of chords
 //  Created by Benjamin Culkin on 12/1/14.
 //  Copyright (c) 2014 bculkin. All rights reserved.
 //
 
 #include <stdio.h>
 
+// Do command processing for an initial character
+//  Return is non-zero until someone wants to exit
+int handlecom(char c);
+
+// Main input loop
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    
+    // Introduce ourselves
+    printf("Welcome to Chorder Alpha v0\n");
+    printf("Please type ? for more help.\n");
+    
+    // Print the prompt
+    printf("> ");
+    
+    // Run commands based on first character
+    while (handlecom(getchar())) {
+        // Reprint the prompt for the next command
+        printf("> ");
+    }
+    return 0;
+}
+
+// Do command processing for an initial character
+//  Return is non-zero until someone wants to exit
+int handlecom(char c) {
+    // If nobody does anything, nothing happens
     return 0;
 }
